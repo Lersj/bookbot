f66ccb065ae31ab3e03f8c328339fdfaa2415d15
Lersj <larsbliksvrhansen@gmail.com>
2025-01-12 13:58:53 +0100

    Aggregated the print commandso it looks nice.

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ def main():
     text = get_book_text(book_path)
     print(text)
     word_count = count_words(text)
-    print("--------------------")
-    print("")
+    items_list = list(count_characters(text).items()) # Converts the dictionary into a list.
+
+    sorted_items_list = sorted(items_list, key=lambda x: x[1], reverse=True) # Sorts the list by the count of characters.
+
+    print(f"--- Book report of {book_path} ---")
+    print()
     print(f"Word count: {word_count}.")
-    print("")
-    print(f"{count_characters(text)}")
+    print()
+    print("Character count:")
+    print()
+    for item in sorted_items_list:
+        if item[0].isalpha(): # Checks if the character is a letter.
+            
+            print(f"There are {item[1]} {item[0]}'s.")
+    print()
+    print("--- End of book report ---")
+    
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -26,6 +39,8 @@ def count_characters(text):
         else:
             character_count_dict[character] = 1
     return character_count_dict
+
+
         
 
 main()
