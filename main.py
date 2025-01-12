9f53ad8b4d4a87da88e3f2d4bfc9a469c5bf8447
Lersj <larsbliksvrhansen@gmail.com>
2025-01-12 13:15:45 +0100

    Added f count words and f count characters dict to main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    word_count = count_words(text)
+    print("--------------------")
+    print("")
+    print(f"Word count: {word_count}.")
+    print("")
+    print(f"{count_characters(text)}")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    return len(text.split())
+
+def count_characters(text):
+    character_count_dict = {}
+    lowered_text = text.lower()
+    
+    for character in lowered_text:
+        if character in character_count_dict:
+            character_count_dict[character] += 1
+        else:
+            character_count_dict[character] = 1
+    return character_count_dict
+        
+
+main()
